63a92550f754abef101af27e7878b007479abaf0
Ricky Thoudam <rickythoudam123@gmail.com>
2025-06-01 23:23:51 +0530

    routes,models,and database connection implemented

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
 const app = express();
 
-app.get('/',(req,res)=>console.log('Working'))
+// Connect database
+connectDB();
 
+app.get('/',(req,res)=>res.send('Working'))
+app.get('/profile',(req,res)=>res.send('This is profile'))
+
+// Define route
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
 
 
 const PORT = process.env.PORT || 4000;
